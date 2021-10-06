021cd5ed427da4824df401f5452bca1bbba6eb7e
Zain Ul Ebad <p166024@nu.edu.pk>
2021-10-07 02:00:19 +0500

    Project Initiated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// eslint-disable-next-line
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch></Switch>
+    </div>
+  );
+}
+
+export default App;
